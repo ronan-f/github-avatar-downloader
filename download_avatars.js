f4dedf23a00bf04e1efc192a8d5c592f293e570a
Ronan Fegan <rfegan13@gmail.com>
2019-03-11 19:26:10 +0000

    updated gitignore and added authentication

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
+var secrets = require('./secrets');
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
